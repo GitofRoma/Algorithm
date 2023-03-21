70744628cb0619d67d30e689b5fb3a4e1b5491e3
Roman.Aleksandrov <roman.trade.in@gmail.com>
2023-03-21 10:24:51 +0100

    homework2+

--- src/homeWork2/Task1.java
+++ src/homeWork2/Task1.java
@@ -53,6 +53,22 @@ public class Task1 {
 
         //   (log N)
 
-
+        String[] words = {"flow", "florida", "cat"};
+        String answer = words[0];
+        for (int i = 1; i < words.length; i++) {
+            answer = prefix(answer, words[i]);
+        }
+        System.out.println(answer);
+    }
+    private static String prefix(String first, String second) {
+        String answer = "";
+        for (int i = 0; i < Math.min(first.length(), second.length()); i++) {
+            if (first.charAt(i) == second.charAt(i)) {
+                answer += first.charAt(i);
+            } else {
+                return answer;
+            }
+        }
+        return answer;
     }
 }
